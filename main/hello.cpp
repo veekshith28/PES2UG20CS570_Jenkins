a87ec049ab328188a875cb3a424be7bd5da58faf
veekshith28 <97212714+veekshith28@users.noreply.github.com>
2023-02-18 10:25:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, THIS IS PES2UG20CS570 \n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully committed changes\n";
   return 0;
 }
