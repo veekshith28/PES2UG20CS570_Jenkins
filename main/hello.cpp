fb382f3f868eb4832a24ebd0b1eae6b1d81459a3
veekshith28 <97212714+veekshith28@users.noreply.github.com>
2023-02-18 09:44:20 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, THIS IS PES2UG20CS570 \n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
